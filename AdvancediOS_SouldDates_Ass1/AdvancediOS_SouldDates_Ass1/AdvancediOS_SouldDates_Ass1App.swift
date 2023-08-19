94d3f8172c3b9b63f464fa6236b8cd9e5e84ad27
choc0099 <40311640+choc0099@users.noreply.github.com>
2023-08-19 20:37:42 +1000

    Initial Commit

---
+++ AdvancediOS_SouldDates_Ass1/AdvancediOS_SouldDates_Ass1/AdvancediOS_SouldDates_Ass1App.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancediOS_SouldDates_Ass1App.swift
+//  AdvancediOS_SouldDates_Ass1
+//
+//  Created by Christopher Averkos on 19/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct AdvancediOS_SouldDates_Ass1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
