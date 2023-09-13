55eb1312dd8639789b59af45d7fedf2fdf970103
choc0099 <40311640+choc0099@users.noreply.github.com>
2023-09-14 06:45:59 +1000

    Added background check related information onto the profile view
    Not yet tested because it has not been declared yet.

--- AdvancediOS_SouldDates_Ass1/AdvancediOS_SouldDates_Ass1/Views/ProfileView.swift
+++ AdvancediOS_SouldDates_Ass1/AdvancediOS_SouldDates_Ass1/Views/ProfileView.swift
@@ -17,6 +17,12 @@ struct ProfileView: View {
     @State private var showAlert: Bool = false
     @State private var alertTitle: String = ""
     @State private var dreamListStatus: String = ""
+    //this is the constants that will store a system image names
+    private let scamIcon: String = "exclamationmark.triangle.fill"
+    private let backgroundCheckIcon: String = "checkmark.seal"
+    //these are the colors being used for background check and scammer status using a custom text view
+    private let scammerBackgroundColour: Color = .red
+    private let backgroundCheckBackgroundColour: Color =  Color("GreenColour")
     
     var body: some View {
         
@@ -35,8 +41,26 @@ struct ProfileView: View {
                     
                     
                     Spacer()
+                    
+                    //a group of text relating to background check statues
+                    Group {
+                        if let haveBackgroundCheck = matchSeeker.backgroundCheck {
+                            if haveBackgroundCheck.policeCheck != nil {
+                                BackgroundCheckStatusView(text: "This match seeker has been policed checked", backgroundColor: backgroundCheckBackgroundColour, systemIconName: backgroundCheckIcon)
+                            }
+                            if haveBackgroundCheck.proofOfAge != nil {
+                                BackgroundCheckStatusView(text: "This match seeker has been verified as a real person.", backgroundColor: backgroundCheckBackgroundColour, systemIconName: backgroundCheckIcon)
+                            }
+                            if haveBackgroundCheck.refereeCheck != nil {
+                                BackgroundCheckStatusView(text: "This match seeker has been referee checked", backgroundColor: backgroundCheckBackgroundColour, systemIconName: backgroundCheckIcon)
+                            }
+                        }
+                    }
+                    
+                    
+                    //checks if it has been reported as a scammer.
                     if matchSeeker.isScammer {
-                        Text("This person was known to be a scammer.").padding().fontWeight(.bold).background(.red).foregroundColor(.white)
+                        BackgroundCheckStatusView(text: "This match seeker was known to be a scammer.", backgroundColor: scammerBackgroundColour, systemIconName: scamIcon)
                     }
                     //Text("Gender: \(matchSeeker.gender.rawValue.capitalized)")
                     Group {
@@ -117,6 +141,6 @@ struct ProfileView: View {
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileView(matchSeeker: matchSeekersSample[1], selectedTab: .constant(.look) )
+        ProfileView(matchSeeker: matchSeekersSample[1], selectedTab: .constant(.look) ).environmentObject(Session()).environmentObject(SoulDatesMain())
     }
 }
